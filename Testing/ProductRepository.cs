44b9c1f6f93b768cf5f5f702fbf06235624ec89e
jmmascorro <jessemascorro88@gmail.com>
2022-04-16 14:51:38 -0500

    Finished Create Product(4) & Delete Product(5)

--- Testing/ProductRepository.cs
+++ Testing/ProductRepository.cs
@@ -22,7 +22,7 @@ namespace Testing
 
         public Product GetProduct(int id)
         {
-            return _conn.QuerySingle<Product>("SELECT * FROM PRODUCTS WHERE PRODUCTID = @id", 
+            return _conn.QuerySingle<Product>("SELECT * FROM PRODUCTS WHERE PRODUCTID = @id",
                 new { id = id });
         }
 
@@ -32,6 +32,36 @@ namespace Testing
                 new { name = product.Name, price = product.Price, id = product.ProductID });
         }
 
-        
+        public void InsertProduct(Product productToInsert)
+        {
+            _conn.Execute("INSERT INTO products (NAME, PRICE, CATEGORYID) VALUES (@name, @price, @categoryID);",
+                new { name = productToInsert.Name, price = productToInsert.Price, categoryID = productToInsert.CategoryID });
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            return _conn.Query<Category>("SELECT * FROM categories;");
+        }
+
+        public Product AssignCategory()
+        {
+            var categoryList = GetCategories();
+            var product = new Product();
+            product.Categories = categoryList;
+
+            return product;
+        }
+
+        public void DeleteProduct(Product product)
+        {
+            _conn.Execute("DELETE FROM REVIEWS WHERE ProductID = @id",
+                new { id = product.ProductID });
+
+            _conn.Execute("DELETE FROM SALES WHERE ProductID = @id",
+                new { id = product.ProductID });
+
+            _conn.Execute("DELETE FROM Products WHERE ProductID = @id",
+                new { id = product.ProductID });
+        }
     }
 }
